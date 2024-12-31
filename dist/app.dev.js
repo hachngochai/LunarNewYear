4b036eec185bb659493069bbc76bae924fea95ea
Hach Ngoc Hai <kiotarot@gmail.com>
2025-01-01 03:04:01 +0700

    new year

---
+++ dist/app.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var fut = new Date("jan 29, 2025 0:00:00").getTime();
+var x = setInterval(function stime() {
+  var now = new Date().getTime();
+  var D = fut - now;
+  var days = Math.floor(D / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(D / (1000 * 60 * 60));
+  var minutes = Math.floor(D / (1000 * 60));
+  var seconds = Math.floor(D / 1000);
+  hours %= 24;
+  minutes %= 60;
+  seconds %= 60;
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (D < 0) {
+    clearInterval(x);
+    window.location.href = "intro.html";
+  }
+}, 0.1);
+//# sourceMappingURL=app.dev.js.map
